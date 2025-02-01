9808fb092b45b183a49e0747e02170bf3be3a0cb
Rohit Chatterjee <rohit@rootbridgeservices.in>
2025-02-01 07:46:07 +0530

    fix build

--- models/intermediate/villagetracker.sql
+++ models/intermediate/villagetracker.sql
@@ -8,6 +8,5 @@
       name AS villagetracker_id, 
       vill_entity AS villageentity_id, 
       date_survey AS datesurvey,
-      track_village AS track_village,
       idx AS idx
 FROM {{source('inrem','VillageTracker')}}
